cd45084b231b56cdf54209227ee8155d269d6f78
MesutRuzgar <mesutruzgar@gmail.com>
2022-03-01 15:49:29 +0300

    Add project files.

---
+++ Singleton/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var customerManager = CustomerManager.CreateAsSingleton();
+            customerManager.Save();
+        }
+
+        class CustomerManager
+        {
+            static CustomerManager _customerManager;
+            private CustomerManager()
+            {
+                    
+            }
+
+            public static CustomerManager CreateAsSingleton()
+            {
+                //klasik yöntem
+                //if (_customerManager==null)
+                //{
+                //    _customerManager = new CustomerManager();
+                //}
+                //return _customerManager;
+
+                //daha pratik bir yöntem
+                return _customerManager ?? (_customerManager = new CustomerManager());
+            }
+            public  void Save()
+            {
+                Console.WriteLine("Saved!");
+            }
+        }
+    }
+}

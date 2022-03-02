13be03c4f513792c87f1c58e2a259134e8ed1f77
MesutRuzgar <mesutruzgar@gmail.com>
2022-03-02 13:56:55 +0300

    Memento example

---
+++ Memento/Program.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Memento
+{
+    class Program
+    {
+        //bir nesne değişikliğe uğradığında arzu edilirse değişikliğe
+        //uğramamış halini geri getirip kullanmamak için kullanılır genel olarak.
+        static void Main(string[] args)
+        {
+
+            Book book = new Book
+            {
+                Isbn = "12345",
+                Title = "Sefiller",
+                Author = "Victor Hugo"
+            };
+            book.ShowBook();
+
+            //hafızaya attık
+            CareTaker history = new CareTaker();
+            history.Memento = book.CreateUndo();
+
+            //veri değiştik
+            book.Isbn = "54321";
+            book.Title = "VICTOR HUGO";
+
+            book.ShowBook();
+            //geri aldık
+            book.RestoreFromUndo(history.Memento);
+            book.ShowBook();
+            Console.ReadLine();
+
+        }
+    }
+    class Book
+    {
+        private string _title;
+        private string _author;
+        private string _isbn;
+        private DateTime _lastEdited;
+
+        public string Title 
+        { get
+            {
+                return _title;
+            }
+            set
+            {
+                _title = value;
+                SetLastEdited();
+            }
+        }
+        public string Author
+        {
+            get
+            {
+                return _author;
+            }
+            set
+            {
+                _author = value;
+                SetLastEdited();
+            }
+        }
+        public string Isbn
+        
+        {
+            get
+            {
+                return _isbn;
+            }
+            set
+            {
+                _isbn = value;
+                SetLastEdited();
+            }
+        }
+        private void SetLastEdited()
+        {
+            _lastEdited = DateTime.UtcNow;
+        }
+
+       public Memento CreateUndo()
+        {
+            return new Memento(_isbn, _title, _author, _lastEdited);
+        }
+        public void RestoreFromUndo(Memento memento)
+        {
+            _title = memento.Title;
+            _author = memento.Author;
+            _isbn = memento.Isbn;
+            _lastEdited = memento.LastEdited;
+        }
+        public void ShowBook()
+        {
+            Console.WriteLine("{0},{1},{2} edited : {3},",Isbn,Title,Author,_lastEdited);
+        }
+    }
+    class Memento
+    {
+        public string Isbn { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public DateTime LastEdited { get; set; }
+
+        public Memento(string isbn,string title,string author,DateTime lastEdited)
+        {
+            Isbn = isbn;
+            Title = title;
+            Author = author;
+            LastEdited = lastEdited;
+        }
+    }
+
+    class CareTaker
+    {
+        public Memento Memento { get; set; }
+    }
+}

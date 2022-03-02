dd5c135dff1cbabbb4de41b67cf70b370f0e1706
MesutRuzgar <mesutruzgar@gmail.com>
2022-03-02 11:09:13 +0300

    Adapter example

---
+++ Adapter/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManager = new ProductManager(new Log4NetAdapter());
+            productManager.Save();
+            Console.ReadLine();
+        }
+    }
+    class ProductManager
+    {
+        ILogger _logger;
+
+        public ProductManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            _logger.Log("User Data");
+            Console.WriteLine("Saved!");
+        }
+    }
+    interface ILogger
+    {
+        void Log(string message);
+    }
+    class MrLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine("Logged, {0}",message);
+        }
+    }
+
+    //nugetten indirdik varsayalım ve dokunamıyoruz bu koda
+    class Log4Net 
+    {
+        public void LogMessage(string message)
+        {
+            Console.WriteLine("Logged with Log4Net, {0}", message);
+        }
+    }
+    //koda dokunamadığımız için bir adaptör oluşturarak onu implemente etmiş olduk
+    class Log4NetAdapter : ILogger
+    {
+        public void Log(string message)
+        {
+            Log4Net log4Net = new Log4Net();
+            log4Net.LogMessage(message);
+        }
+    }
+}
